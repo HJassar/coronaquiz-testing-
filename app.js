3a6443a5ae16bd5ee4d567bdb60a2e54baae35c1
Hamza <jassarhamza@gmail.com>
2020-03-18 22:25:42 -0700

    trying local referrer

--- app.js
+++ app.js
@@ -58,10 +58,10 @@ app.get("/:rating",(req,res)=>{
         case `knlihuu55h`    : ogImg = "limited"; message = `معلوماتك عن فايروس الكورونا محدودة، لربما ترغب بـ<a href="/">ـالمحاولة مرة أخرى</a>.`; break;
         case `i98ouasd`      : ogImg = "weak"; message = `معلوماتك عن فايروس الكورونا ضعيفة جدا، ننصحك بالاطلاع على المزيد من المصادر. لربما ترغب بـ<a href="/">ـالمحاولة مرة أخرى</a>.`; break;
     }
-    if(!score || req.headers.referer!= process.env.REFERER ){
+    if(!score){
         res.redirect("/")
 }else{
-    res.render("result",{score:score, message:message, rating:rating, ogImg:ogImg});
+    res.render("result",{score:score, message:message, rating:rating, ogImg:ogImg, referer:process.env.REFERER});
 }
     })
 
